d9404b63e4653831b41d572446b4c1873ace1ee1
Fred Fucks <maryjaneinchain@gmail.com>
2024-08-20 19:23:57 +0800

    init

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Counter from '../contracts/counter';
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { Address, OpenedContract } from "@ton/core";
+import { useTonConnect } from "./useTonConnect";
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | number>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse('EQDIpvVzx_E5a3UBBjYn7QdMRNzM0jqGb1Yq0GnA010ULTKv')
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(Number(val));
+      await sleep(5000);
+      getValue();
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
+  };
+}
